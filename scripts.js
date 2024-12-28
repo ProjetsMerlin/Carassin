576aeb3ed9d5d1269d962d240ca9481326f02e3a
ProjetsMerlin <infolintermediaire@gmail.com>
2024-12-28 11:37:17 +0100

    add tv

--- scripts.js
+++ scripts.js
@@ -65,7 +65,7 @@ function degreeToCardinal(deg) {
 }
 
 server = 'server-sample.php'
-if ( window.location.hostname === 'localhost' ) { //lintermediaire.be
+if ( window.location.hostname === 'lintermediaire.be' ) {
   var server = 'server.php'
 }
 
